dfd0bdb79906ca129fe066c9712095e1c1a4d802
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-11 08:52:26 +0200

    Divisions program som omvandlar datatyper

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Uppgift A");
+
+        Scanner tal = new Scanner(System.in);
+        System.out.print("Ange tal 1: ");
+        int tal1 = tal.nextInt();
+
+        System.out.print("Ange tal 2: ");
+        int tal2 = tal.nextInt();
+
+        double kvot = (double)tal1/(double)tal2;
+        System.out.println("Kvoten är "+kvot);
+
+        int kvot2=(int)tal1/(int)tal2;
+        System.out.println("Kvoten är: "+kvot2);
     }
 }
