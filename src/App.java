69057cb50051700a648994419adbe4412aebf43c
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-09-11 09:02:06 +0200

    ändra substantv

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Uppgift A");
+        System.out.println("Uppgift A - Divisions program:");
 
         Scanner tal = new Scanner(System.in);
         System.out.print("Ange tal 1: ");
@@ -16,5 +16,18 @@ public class App {
 
         int kvot2=(int)tal1/(int)tal2;
         System.out.println("Kvoten är: "+kvot2);
+        
+        System.out.println("Uppgift B - Substantiv");
+
+        Scanner tb = new Scanner(System.in);
+        System.out.print("Ange ett substantiv: " );
+        String sub = tb.nextLine();
+
+        System.out.print("Ange ett substantivets pluraländelse: " );
+        String subend = tb.nextLine();
+
+        System.out.println(sub+""+subend);
+
+
     }
 }
